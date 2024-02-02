6a378231d0ab7747f87c11287edbedf2fd29081a
vekandal <sarada.kandala@gmail.com>
2024-02-03 00:58:16 +0530

    mongodb.sh sed correction

--- mongodb.sh
+++ mongodb.sh
@@ -5,5 +5,6 @@ systemctl enable mongod
 systemctl start mongod
 
 #modify local host from 127.0.0.1 to 0.0.0.0 using shell
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 
 systemctl restart mongod
