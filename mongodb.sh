ea5c34094d4ddce79031788177c05e776332eb6c
vekandal <sarada.kandala@gmail.com>
2024-02-05 12:57:23 +0530

    front end copy command correction

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,14 @@
+echo -e "************\e[36m Copy mongo repo **********\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "************\e[36m Install mongodb **********\e[0m"
 dnf install mongodb-org -y
 
+echo -e "************\e[36m enable and start mongodb **********\e[0m"
 systemctl enable mongod
 systemctl start mongod
 
+echo -e "************\e[36m enable ip to listen from all hosts **********\e[0m"
 #modify local host from 127.0.0.1 to 0.0.0.0 using shell
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-
+echo -e "************\e[36m Restart mongodb **********\e[0m"
 systemctl restart mongod
